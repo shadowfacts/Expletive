fae0ac0e74f09bcfa3c69cf4006fd7177ff0341f
Shadowfacts <me@shadowfacts.net>
2016-10-27 16:52:28 -0400

    ResourcePackCategory

--- src/main/kotlin/net/shadowfacts/expletive/model/resourcepack/ResourcePackCategory.kt
+++ src/main/kotlin/net/shadowfacts/expletive/model/resourcepack/ResourcePackCategory.kt
@@ -3,5 +3,25 @@ package net.shadowfacts.expletive.model.resourcepack
 /**
  * @author shadowfacts
  */
-enum class ResourcePackCategory {
+enum class ResourcePackCategory(val displayName: String) {
+
+	ALL("All"),
+	SIXTEEN_X("16x"),
+	THIRTY_TWO_X("32x"),
+	SIXTY_FOUR_X("64x"),
+	ONE_TWENTY_EIGHT_X("128x"),
+	TWO_FIFTY_SIX_X("256x"),
+	FIVE_TWELVE_X_AND_BEYOND("512x and Higher"),
+	ANIMATED("Animated"),
+	MEDIEVAL("Medieval"),
+	MOD_SUPPORT("Mod Support"),
+	MODERN("Modenr"),
+	PHOTO_REALISTIC("Photo Realistic"),
+	STEAMPUNK("Steampunk"),
+	TRADITIONAL("Traditional"),
+	MISC("Miscellaneous");
+
+	val url: String
+		get() = if (this == ALL) "" else ("/" + name.toLowerCase().replace('_', '-'))
+
 }
